614045ce129589c3518262388d87cee418f72665
Nikita <39771918+NikitaGura@users.noreply.github.com>
2021-05-06 21:07:52 +0200

    added joystick for shooting
    Removed shooting by touch

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -6,68 +6,37 @@ public class Weapon : MonoBehaviour
     public GameObject projectile;
     public Transform shotPoint;
     public Player player;
+    public Joystick joystick;
 
     private float timeBetweenShots = 0; 
     public float startTimeBetweenShots;
 
-    private void Start() {
-        //player = GetComponent<Player>();  
-    }
-    
     private void Update()
     {
         timeBetweenShots -= Time.deltaTime;
-        if (Input.touchCount > 0 )
-        {
-            for ( var i = 0 ; i < Input.touchCount ; i++ ) {
-                var touch = Input.GetTouch(i);
-
-                if (EventSystem.current.IsPointerOverGameObject(touch.fingerId)){
-               
-                } else { 
-                     switch (touch.phase)
-                        {
-                            case TouchPhase.Began:
-                            case TouchPhase.Moved:
-                            case TouchPhase.Stationary:
+        
+        if(Mathf.Abs(joystick.Vertical) > 0.3f || Mathf.Abs(joystick.Horizontal) > 0.3f){
+              player.isShooting = true;
+              float rotZ = Mathf.Atan2(joystick.Vertical, joystick.Horizontal) * Mathf.Rad2Deg;
                                 
-                                player.isShooting = true;
-
-                                Vector3 difference = Camera.main.ScreenToWorldPoint(touch.position) - transform.position;
-                                float rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
-
-                                if(player.facingRight && difference.x < 0) {
-                                    player.Flip();
-                                } else if (!player.facingRight && difference.x > 0) {
-                                    player.Flip();
-                                    
-                                }
-                                
-                                if (rotZ < -90 || rotZ > 90) {
-                                    transform.rotation = Quaternion.Euler(180f, 0f, -rotZ);
-                                }else {
-                                    transform.rotation = Quaternion.Euler(0f, 0f, rotZ);
-                                }
-                               
-
-                                if ( timeBetweenShots <= 0 ){
-                                       Instantiate(projectile, shotPoint.position,  transform.rotation);
-                                       timeBetweenShots = startTimeBetweenShots;
-                                }
-                                
-                              
-
-                                break;
-
-                            case TouchPhase.Canceled:
-                            case TouchPhase.Ended:
-                            player.isShooting = false ;
-                                break;
-                        }
+              if (rotZ < -90 || rotZ > 90) {
+                    transform.rotation = Quaternion.Euler(180f, 0f, -rotZ);
+                    if(player.facingRight ) {
+                            player.Flip();
+                    }
+                }else {
+                    transform.rotation = Quaternion.Euler(0f, 0f, rotZ);
+                    if(!player.facingRight ) {
+                            player.Flip();
+                    }
                 }
-
-            }
-           
+                
+                if ( timeBetweenShots <= 0 ){
+                    Instantiate(projectile, shotPoint.position,  transform.rotation);
+                    timeBetweenShots = startTimeBetweenShots;
+                }
+        }else{
+            player.isShooting = false;
         }
     }
 }
